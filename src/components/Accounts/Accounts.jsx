c62a438d2822f0b320025ecc717fb81b33f7241f
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-06-29 15:12:38 +0200

    Create new repository and add a layout fo whole project

---
+++ src/components/Accounts/Accounts.jsx
@@ -0,0 +1,25 @@
+import GitHub from "./images/GitHub.png";
+import LinkdedIn from "./images/LinkedIn.png";
+import "./Accounts.scss";
+import { useTranslation } from "react-i18next";
+
+export const Accounts = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={"accounts"}>
+      <p>{t("accounts")}</p>
+      <a target={"_blank"} href={"https://github.com/WeronikaCzepiela"}>
+        {" "}
+        <img src={GitHub} />{" "}
+      </a>
+      <a
+        target={"_blank"}
+        href={"https://www.linkedin.com/in/--weronika-czepiela/"}
+      >
+        {" "}
+        <img src={LinkdedIn} />
+      </a>
+    </div>
+  );
+};
