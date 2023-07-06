cf2b163b17ac3fd9757fcf727304c65c07a06656
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-07-06 22:33:56 +0200

    Start creating function for counting the score

--- src/components/GameItem/GameItem.jsx
+++ src/components/GameItem/GameItem.jsx
@@ -1,15 +1,20 @@
 import './GameItem.scss'
 import { useWindowDimensions } from '../../utils/hookes/useWindowDimensions'
 import KnifeAndFork from '../Icons/KnifeAndFork'
+import { useState } from 'react'
 
 export const GameItem = (idx, order, vector, complete, clicked) => {
-  const handleOnClick = () => {}
+  const [score, setScore] = useState(0)
+  const handleOnClick = (score) => {
+    setScore(score + 1)
+  }
+  console.log(score)
 
   const { width } = useWindowDimensions()
   return (
     <div className={`item ${width > 600 && 'item-desktop'}`} onClick={handleOnClick}>
       {/*<div className={`icon ${width > 600 && 'icon-dektop'}`}>*/}
-      <KnifeAndFork className={'dupa'} />
+      {/*<KnifeAndFork className={'dupa'} />*/}
       {/*</div>*/}
     </div>
   )
