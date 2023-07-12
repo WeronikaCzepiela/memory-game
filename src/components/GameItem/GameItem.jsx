c0d1cb2f006e10147d1e7f147d4b6bfcea24b64b
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-07-12 12:30:55 +0200

    Add icons as jsx files

--- src/components/GameItem/GameItem.jsx
+++ src/components/GameItem/GameItem.jsx
@@ -1,5 +1,36 @@
 import './GameItem.scss'
 import { useWindowDimensions } from '../../utils/hookes/useWindowDimensions'
+import KnifeAndFork from '../Icons/Ghost'
+import Ghost from '../Icons/Ghost'
+import PuzzlePiece from '../Icons/PuzzlePiece'
+import HatWizzard from '../Icons/HatWizzard'
+import ChessKnight from '../Icons/ChessKnight'
+import ChessRook from '../Icons/ChessRook'
+import Hippo from '../Icons/Hippo'
+import Poo from '../Icons/Poo'
+import Umbrella from '../Icons/Umbrella'
+import Droplet from '../Icons/Droplet'
+import MugSaucer from '../Icons/MugSaucer'
+import Bug from '../Icons/Bug'
+import UserSecret from '../Icons/UserSecret'
+import Anchor from '../Icons/Anchor'
+import Leaf from '../Icons/Leaf'
+import Virus from '../Icons/Virus'
+import Dragon from '../Icons/Dragon'
+import LightBulb from '../Icons/LightBulb'
+import TruckMonster from '../Icons/TruckMonster'
+import WineGlass from '../Icons/WineGlass'
+import Vial from '../Icons/Vial'
+import Volcano from '../Icons/Volcano'
+import Suitcase from '../Icons/Suitcase'
+import Socks from '../Icons/Socks'
+import Snowman from '../Icons/Snowman'
+import ShoePrints from '../Icons/ShoePrints'
+import SackDollar from '../Icons/SackDollar'
+import PiggyBank from '../Icons/PiggyBank'
+import PizzaSlice from '../Icons/PizzaSlice'
+import Paw from '../Icons/Paw'
+import Meteor from '../Icons/Meteor'
 
 export const GameItem = ({ id, order, vector, complete, clicked, onClick }) => {
   const { width } = useWindowDimensions()
@@ -14,7 +45,13 @@ export const GameItem = ({ id, order, vector, complete, clicked, onClick }) => {
         complete ? 'item-complete' : ''
       }`}
       onClick={handleOnClick}>
-      {vector}
+      <div>
+        <Meteor width={50} height={50} fill={'white'} />
+      </div>
     </div>
   )
 }
+
+// className={`vector ${clicked ? 'vector-clicked' : ''} ${
+//     complete ? 'vector-complete' : ''
+// }`}
