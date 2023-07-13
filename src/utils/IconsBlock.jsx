43cf14dead0b78b8cddf1dbad4667b4a2654beba
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-07-13 13:03:02 +0200

    Add new file with an array of icons

---
+++ src/utils/IconsBlock.jsx
@@ -0,0 +1,63 @@
+import Poo from '../components/Icons/Poo'
+import Anchor from '../components/Icons/Anchor'
+import Bug from '../components/Icons/Bug'
+import ChessKnight from '../components/Icons/ChessKnight'
+import ChessRook from '../components/Icons/ChessRook'
+import Dragon from '../components/Icons/Dragon'
+import Droplet from '../components/Icons/Droplet'
+import Ghost from '../components/Icons/Ghost'
+import HatWizard from '../components/Icons/HatWizard'
+import Hippo from '../components/Icons/Hippo'
+import Leaf from '../components/Icons/Leaf'
+import LightBulb from '../components/Icons/LightBulb'
+import Meteor from '../components/Icons/Meteor'
+import MugSaucer from '../components/Icons/MugSaucer'
+import Paw from '../components/Icons/Paw'
+import PiggyBank from '../components/Icons/PiggyBank'
+import PizzaSlice from '../components/Icons/PizzaSlice'
+import PuzzlePiece from '../components/Icons/PuzzlePiece'
+import SackDollar from '../components/Icons/SackDollar'
+import ShoePrints from '../components/Icons/ShoePrints'
+import Snowman from '../components/Icons/Snowman'
+import Socks from '../components/Icons/Socks'
+import Suitcase from '../components/Icons/Suitcase'
+import TruckMonster from '../components/Icons/TruckMonster'
+import Umbrella from '../components/Icons/Umbrella'
+import UserSecret from '../components/Icons/UserSecret'
+import Vial from '../components/Icons/Vial'
+import Virus from '../components/Icons/Virus'
+import Volcano from '../components/Icons/Volcano'
+import WineGlass from '../components/Icons/WineGlass'
+
+export const vectorIcons = [
+  { vector: 'anchor', icon: <Anchor width={50} height={50} fill={'white'} /> },
+  { vector: 'bug', icon: <Bug width={50} height={50} fill={'white'} /> },
+  { vector: 'chess-knight', icon: <ChessKnight width={50} height={50} fill={'white'} /> },
+  { vector: 'chess-rook', icon: <ChessRook width={50} height={50} fill={'white'} /> },
+  { vector: 'dragon', icon: <Dragon width={50} height={50} fill={'white'} /> },
+  { vector: 'droplet', icon: <Droplet width={50} height={50} fill={'white'} /> },
+  { vector: 'ghost', icon: <Ghost width={50} height={50} fill={'white'} /> },
+  { vector: 'hat-wizard', icon: <HatWizard width={50} height={50} fill={'white'} /> },
+  { vector: 'hippo', icon: <Hippo width={50} height={50} fill={'white'} /> },
+  { vector: 'leaf', icon: <Leaf width={50} height={50} fill={'white'} /> },
+  { vector: 'light-bulb', icon: <LightBulb width={50} height={50} fill={'white'} /> },
+  { vector: 'meteor', icon: <Meteor width={50} height={50} fill={'white'} /> },
+  { vector: 'mug-saucer', icon: <MugSaucer width={50} height={50} fill={'white'} /> },
+  { vector: 'paw', icon: <Paw width={50} height={50} fill={'white'} /> },
+  { vector: 'piggy-bank', icon: <PiggyBank width={50} height={50} fill={'white'} /> },
+  { vector: 'pizza-slice', icon: <PizzaSlice width={50} height={50} fill={'white'} /> },
+  { vector: 'poo', icon: <Poo width={50} height={50} fill={'white'} /> },
+  { vector: 'puzzle-piece', icon: <PuzzlePiece width={50} height={50} fill={'white'} /> },
+  { vector: 'sack-dollar', icon: <SackDollar width={50} height={50} fill={'white'} /> },
+  { vector: 'shoe-prints', icon: <ShoePrints width={50} height={50} fill={'white'} /> },
+  { vector: 'snowman', icon: <Snowman width={50} height={50} fill={'white'} /> },
+  { vector: 'socks', icon: <Socks width={50} height={50} fill={'white'} /> },
+  { vector: 'suitcase', icon: <Suitcase width={50} height={50} fill={'white'} /> },
+  { vector: 'truck-monster', icon: <TruckMonster width={50} height={50} fill={'white'} /> },
+  { vector: 'umbrella', icon: <Umbrella width={50} height={50} fill={'white'} /> },
+  { vector: 'user-secret', icon: <UserSecret width={50} height={50} fill={'white'} /> },
+  { vector: 'vial', icon: <Vial width={50} height={50} fill={'white'} /> },
+  { vector: 'virus', icon: <Virus width={50} height={50} fill={'white'} /> },
+  { vector: 'volcano', icon: <Volcano width={50} height={50} fill={'white'} /> },
+  { vector: 'wine-glass', icon: <WineGlass width={50} height={50} fill={'white'} /> },
+]
